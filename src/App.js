f94d030e7ce9b8c13526470365c30a48b517c478
Shane McGuire <shanelgmcguire@gmail.com>
2020-07-19 22:51:36 -0600

    re-arranged code to work with router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React from 'react';
 import shnLogo from './images/logo1.png'
 import './App.css';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
 import { ScrollBar } from './components/scrollBar/ScrollBar'
-import { Sections } from './components/sections/Sections'
+import { WorkPage } from './components/sections/work/WorkPage'
+import { About } from './components/sections/about/About'
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <img src={shnLogo} alt='SHN'/>
-      </header>
-      <main>
-        <ScrollBar/>
-        <Sections/>
-        <div className='sectionDetails'>
-          <div>
-            Image container
+    <Router>
+      <div className="App">
+        <header>
+          <img src={shnLogo} alt='SHN'/>
+        </header>
+        <nav>
+          <ScrollBar/>
+        </nav>
+        <main>
+          <Switch>
+            <Route exact path ='/' component={About}/>
+            <Route path='/work' component={WorkPage}/>
+          </Switch>
+          <div className='sectionDetails'>
+            <div>
+              Image container
+            </div>
+            <div>
+              side scroll
+            </div>
           </div>
-          <div>
-            side scroll
-          </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/sections/work/WorkPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './workPage.css'
+
+export const WorkPage = () => {
+    return (
+        <div className='mainContainer'>
+            <div className='work'>
+                <h2 className='workHeading'>WORK</h2>
+                <span className='pageNumber'>1</span>
+                <p className='workDescription'>
+                Quisque faucibus tincidunt pretium. Aliquam placerat 
+                </p>
+            </div>
+        </div>
+    )
+}
